d74fa9ade15d3abf09786e407a87b3eff75e368f
BeataMaro <beatamaro@interia.pl>
2020-03-02 19:06:11 +0100

    clean css updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,44 @@
 //Loading animation Count down
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-    let preloader = document.querySelector('.preloader');
-    let loader = document.querySelector('.loader');
-    let element = document.querySelector('.counter');
+//     let preloader = document.querySelector('.preloader');
+//     let loader = document.querySelector('.loader');
+//     let element = document.querySelector('.counter');
 
-    let count = 0;
-    let loadingFunction = setInterval(() => {
+//     let count = 0;
+//     let loadingFunction = setInterval(() => {
 
-        if (count < 101) {
-            document.body.style.position = "fixed";
+//         if (count < 101) {
+//             document.body.style.position = "fixed";
 
-            element.textContent = `${count}%`;
-            loader.style.width = `${count}%`;
-            count++
-        } else {
-            clearInterval(loadingFunction)
-            document.body.style.position = "relative";
-            fadeOut(preloader)
-        }
+//             element.textContent = `${count}%`;
+//             loader.style.width = `${count}%`;
+//             count++
+//         } else {
+//             clearInterval(loadingFunction)
+//             document.body.style.position = "relative";
+//             fadeOut(preloader)
+//         }
 
-    }, 10);
+//     }, 10);
 
-    function fadeOut(element) {
+//     function fadeOut(element) {
 
-        element.style.opacity = 1;
+//         element.style.opacity = 1;
 
-        (function fade() {
-            if ((element.style.opacity -= 0.1) < 0) {
-                element.style.display = 'none';
+//         (function fade() {
+//             if ((element.style.opacity -= 0.1) < 0) {
+//                 element.style.display = 'none';
 
-            } else {
-                requestAnimationFrame(fade);
-            }
+//             } else {
+//                 requestAnimationFrame(fade);
+//             }
 
 
-        })();
-    }
-});
+//         })();
+//     }
+// });
 
 /***/
 
